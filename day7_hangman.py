fb5ad9f1149582df04d141e0237a4388baba5cf1
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2025-08-31 21:21:00 -0400

    day7

---
+++ day7_hangman.py
@@ -0,0 +1,49 @@
+import random
+'''
+words = ["cat", "dog", "book", "car", "apple"]
+word = random.choice(words)
+
+guessed = ""
+tries = 6
+
+print("Welcome to Hangman!")
+
+while tries > 0:
+    # Show the word with underscores and spaces
+    display = ""
+    for letter in word:
+        if letter in guessed:
+            display += letter + " "
+        else:
+            display += "_ "
+    print(display.strip())
+
+    # Check win
+    if display.replace(" ", "") == word:
+        print("You win! The word was:", word)
+        break
+
+    guess = input("Guess a letter: ")
+
+    if guess in word:
+        guessed += guess
+        print("Good guess!")
+    else:
+        tries -= 1
+        print("Wrong! Tries left:", tries)
+
+if tries == 0:
+    print("Game over! The word was:", word)
+'''
+
+word_list = ["artwark", "baboon", "camel"]
+
+word = random.choice(word_list)
+
+guess = input("Guess a letter: ").lower()
+for letter in word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
+
